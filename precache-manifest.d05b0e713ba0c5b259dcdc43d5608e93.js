6136d750de9720f81d76e7a557cbea138f5b046a
tyushnyakov <ktyushnyakov@gmail.com>
2020-10-06 12:41:09 +1000

    Updates

---
+++ precache-manifest.d05b0e713ba0c5b259dcdc43d5608e93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "374e480908a826ef490252e795abf151",
+    "url": "/basics/index.html"
+  },
+  {
+    "revision": "8fbf8c9c848b7b598044",
+    "url": "/basics/static/css/main.af9a434f.chunk.css"
+  },
+  {
+    "revision": "b1abcfcfc9e9c6779319",
+    "url": "/basics/static/js/2.3a497fcf.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/basics/static/js/2.3a497fcf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fbf8c9c848b7b598044",
+    "url": "/basics/static/js/main.2becf531.chunk.js"
+  },
+  {
+    "revision": "5b4cf26e4c19215075f2",
+    "url": "/basics/static/js/runtime-main.8da38d7d.js"
+  }
+]);
